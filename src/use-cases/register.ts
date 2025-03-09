299b5d5b080bd902fc3dbedba53e9d1ec1a67ad3
ricardocr18 <ricardocribeiro@hotmail.com>
2025-03-08 22:04:38 -0300

    create interface de repositorio

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,39 +1,29 @@
-import { prisma } from "@/lib/prisma"
 import { hash } from "bcryptjs"
+import { UsersRepository } from '@/repositories/users-repository'
 
 interface RegisterUseCaseRequest {
-    name: string
+    name: string                    
     email: string
     password: string
 }
 
-export class RegisterUseCase {
-  constructor(private usersRepository: any) {}
+export class RegisterUseCase {  
+  constructor(private usersRepository: UsersRepository) {}
 
-  async execute ({name, email,password}: RegisterUseCaseRequest){
-    // validar e criar um hash para senha
-    const password_hash = await hash(password, 6)
-
-    // aqui vejo se já tem algum email cadastrado
-    const userWithSameEmail = await prisma.user.findUnique({
-      where: {
-        email
-      }
-    })
+  async execute({name, email,password}: RegisterUseCaseRequest){ 
+    const password_hash = await hash(password, 6)    
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
     
-    // se tem e-mail cadastrado eu dou um aviso 
+    
     if (userWithSameEmail){
         throw new Error('E-mail já cadastrado')
-    }
+    }        
 
-    // const prismaUsersRepository = new PrismaUsersRepository()
-    
     await this.usersRepository.create({
-        name,
-        email,
-        password_hash
+      name,
+      email,
+      password_hash,
     })
-    
   }
 
 }
